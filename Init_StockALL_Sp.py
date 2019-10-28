1820c8c8513f103585d14ee70cc2a4f41643efa8
杨振国 <yzg_4164@qq.com>
2019-10-28 17:16:59 +0800

    test

--- Init_StockALL_Sp.py
+++ Init_StockALL_Sp.py
@@ -5,14 +5,14 @@ import pymysql
 if __name__ == '__main__':
 
     # 设置tushare pro的token并获取连接
-    ts.set_token('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+    ts.set_token('009126737252b046badcfa9071d852f55aff537d18d68044fd360453')
     pro = ts.pro_api()
     # 设定获取日线行情的初始日期和终止日期，其中终止日期设定为昨天。
     start_dt = '20100101'
     time_temp = datetime.datetime.now() - datetime.timedelta(days=1)
     end_dt = time_temp.strftime('%Y%m%d')
     # 建立数据库连接,剔除已入库的部分
-    db = pymysql.connect(host='127.0.0.1', user='root', passwd='admin', db='stock', charset='utf8')
+    db = pymysql.connect(host='rm-2zenk50lop6a1jdg4mo.mysql.rds.aliyuncs.com', user='xyt3', passwd='Xyt3@1234', db='test', charset='utf8')
     cursor = db.cursor()
     # 设定需要获取数据的股票池
     stock_pool = ['603912.SH','300666.SZ','300618.SZ','002049.SZ','300672.SZ']
